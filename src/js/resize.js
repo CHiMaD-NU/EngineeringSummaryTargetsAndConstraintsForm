38e4486ea649d2031571da236402a1c0c85991c9
Aaron Geller <a-geller@northwestern.edu>
2020-10-13 15:43:15 -0500

    updated title

--- src/js/resize.js
+++ src/js/resize.js
@@ -1,9 +1,9 @@
 //attach listener on window resize and run it once to start
 window.addEventListener("resize", resizer);
-resizer();
+setTimeout(function(){ resizer();; }, 500); //hack to try to allow time for the image to load (could do this more robustly but that seems more complicated than necessary)
 
 function resizer(){
-
+	console.log('resizing')
 	var lim = 0.1*window.innerHeight; //pixel limit to allow
 	var nLim = 50; //limit the number of iterations for text resizing
 
