c34f80b544064e8cfc98b7b336d3b1b2c8fb2303
Aaron Geller <a-geller@northwestern.edu>
2020-10-13 14:58:57 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# CHiMaDEngineeringSummayTargetsAndConstraintsForm
+# CHiMaD Engineering Summay Targets And Constraints Form
+
+![CHiMaD logo](CHiMaD_Final_wname.png)
+
 Website to hold the form for the CHiMaD training on Targets and Constraints
+
+A live version is available [here](https://ageller.github.io/CHiMaDEngineeringSummayTargetsAndConstraintsForm/).
+
---
+++ src/js/resize.js
@@ -0,0 +1,32 @@
+//attach listener on window resize and run it once to start
+window.addEventListener("resize", resizer);
+resizer();
+
+function resizer(){
+
+	var lim = 0.1*window.innerHeight; //pixel limit to allow
+	var nLim = 50; //limit the number of iterations for text resizing
+
+	//resize the paragraph font size so the para fills the screen height
+	var para = document.getElementById('paraColumn');
+	var fs = parseFloat(window.getComputedStyle(para, null).getPropertyValue('font-size'));
+	var rect = para.getBoundingClientRect();
+	var height = window.innerHeight - rect.top;
+	var diff = height - rect.height;
+	var nTrial = 0;
+	var fac = 0.5;
+
+	while ((Math.abs(diff) > lim || diff < 0) & nTrial < nLim){
+		var mult = fac*(1. + nTrial/nLim);
+		if (diff > 0){
+			mult =  2. - mult;
+		}
+		para.style.fontSize = fs*mult + 'px';
+		fs = parseFloat(window.getComputedStyle(para, null).getPropertyValue('font-size'));
+		rect = para.getBoundingClientRect();
+		diff = height - (rect.height + 0.005*window.innerWidth); //adding to account for of form?
+
+		nTrial += 1
+	}
+	document.getElementById('form').height = rect.height 
+}
